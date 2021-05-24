05c195586da75bce666e4a1ca6c3f41703d1a631
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2021-05-24 16:44:39 -0700

    updated footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import { ReactComponent as CopyRight } from '../../assets/copyRight.svg';
 import { Container } from './styles';
@@ -6,7 +7,7 @@ const Footer: React.FC = () => {
   return (
     <Container>
       <CopyRight />
-      <h2>2020 Fernando Silva</h2>
+      <h2>{new Date().getFullYear()} Fernando Silva</h2>
     </Container>
   );
 };
