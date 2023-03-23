9d5f79601a6b457049e38485ef8f67433a8208fe
Fernandosilvasc <fernando_silvasc@hotmail.com>
2023-03-22 18:36:47 -0700

    - updated the uuid lib

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,4 @@
-import { uuid } from 'uuidv4';
+import { v4 as uuid } from 'uuid';
 
 import CryptoTrackerImg from '../assets/cryptoTracker.png';
 // import CryptoTrackerMockup from '../assets/cryptoTrackerMockup.png';
